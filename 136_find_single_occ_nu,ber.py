9337eb2689acb085d9c3ecc0dd2398d5087f7736
inflictt <legitsaksham@gmail.com>
2025-09-21 23:18:32 +0530

    find_single_number_brute_force

--- 136_find_single_occ_nu,ber.py
+++ 136_find_single_occ_nu,ber.py
@@ -11,3 +11,11 @@
 
 # Output: 1
 nums = [2,2,1]
+#using hash map for our brute for method 
+hashmap={}
+for num in nums:
+    hashmap[num]=hashmap.get(num,0)+1#this checks if the  num index value is in the hashmap if yes it updates the value by +1 and if not it set it as by default 0
+    
+for key in hashmap:
+    if hashmap[key]==1:#check for the value of 1 if there we got our single number
+        print[key]
